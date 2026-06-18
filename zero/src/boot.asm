f4f2e2433a7fa6d30b472f573832397072d42b87
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-06-18 20:00:39 +0900

    fix: boot.asmにCカーネルの呼び出し処理と初期化を追加

--- zero/src/boot.asm
+++ zero/src/boot.asm
@@ -1,8 +1,24 @@
 [bits 16]
-[org 0x7c00]
+
+section .boot
+global start
+extern kernel_main  ; kernel.c の関数を呼び出せるようにする
 
 start:
+    ; セグメントレジスタとスタックの初期化
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7c00
+
+    ; kernel.c のメイン関数を実行
+    call kernel_main
+
+    ; カーネルから戻ってきたら無限ループ
     jmp $
 
+; 512バイトのセクタ末尾にマジックナンバーを配置するためのパディング
+; (リンカで結合するため、timesでの埋め方はビルド環境に合わせる必要があります)
 times 510-($-$$) db 0
 dw 0xaa55
