224933d1d810741e2c89d204f6625a5fe3e2593e
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-06-20 06:28:36 +0900

    確実に32bitようにコンパイルできるように書き直した

--- zero/src/kernel.c
+++ zero/src/kernel.c
@@ -1,17 +1,17 @@
 void kernel_main() {
-    // 画面（VGAバッファ）の住所
+    // 画面（VGAバッファ）の物理アドレス
     unsigned short *video_memory = (unsigned short *)0xb8000;
     
     // 表示したい文字列
     const char *str = "Hello, World!";
     
-    // i番目の文字が '\0'（終端）になるまで繰り返す
+    // i番目の文字が '\0' になるまで繰り返す
     for (int i = 0; str[i] != '\0'; i++) {
-        // 文字を書き込む（0x0a は黄緑色だよ！）
+        // 文字を書き込む（0x0a は黄緑色）
         video_memory[i] = (unsigned short)str[i] | (0x0a << 8);
     }
     
-    // CPUを停止状態にして待機（OSが勝手に終了しないように）
+    // CPUを停止状態にして待機
     while(1) {
         __asm__("hlt");
     }
