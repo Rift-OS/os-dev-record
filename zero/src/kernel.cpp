b25901cdb90be41beee0d9e07e1917c2df0caff6
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-06-20 18:50:23 +0900

    LICENSEヘッダーを追加しました

--- zero/src/kernel.cpp
+++ zero/src/kernel.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright [2026] [Rift-OS Project]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://apache.org
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 extern "C" void kernel_main() {
     // 画面（VGAバッファ）の物理アドレス
     auto *video_memory = reinterpret_cast<unsigned short *>(0xb8000);
