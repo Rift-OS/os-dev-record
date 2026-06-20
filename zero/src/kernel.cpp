63d6718ec201bfbcd504b3704f2e12bfb3c3939f
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-06-20 18:10:33 +0900

    CからC++に書き直した

--- zero/src/kernel.cpp
+++ zero/src/kernel.cpp
@@ -1,14 +1,14 @@
-void kernel_main() {
+extern "C" void kernel_main() {
     // 画面（VGAバッファ）の物理アドレス
-    unsigned short *video_memory = (unsigned short *)0xb8000;
+    auto *video_memory = reinterpret_cast<unsigned short *>(0xb8000);
     
     // 表示したい文字列
-    const char *str = "Hello, World!";
+    const char *str = "Hello, C++ World!";
     
     // i番目の文字が '\0' になるまで繰り返す
     for (int i = 0; str[i] != '\0'; i++) {
         // 文字を書き込む（0x0a は黄緑色）
-        video_memory[i] = (unsigned short)str[i] | (0x0a << 8);
+        video_memory[i] = static_cast<unsigned short>(str[i]) | (0x0a << 8);
     }
     
     // CPUを停止状態にして待機
