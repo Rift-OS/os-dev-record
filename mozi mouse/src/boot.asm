9276fe08fbac618ff0fe0a46b8f95f72c05ef970
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-06-25 17:51:24 +0900

    Update boot.asm

--- mozi mouse/src/boot.asm
+++ mozi mouse/src/boot.asm
@@ -1,15 +1,117 @@
-bits 32
+org 0x7C00
+bits 16
+
+section .boot
+
+boot_start:
+    jmp 0x0000:.init_segments
+
+.init_segments:
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+
+    ; 1. カーネル（2セクタ目以降）を一時的に 0x0000:0x8000 に読み込む
+    ; (15セクタ分読み込み。カーネルが大きくなったらここを増やす)
+    mov ah, 0x02        ; BIOS Read Sectors
+    mov al, 15          ; 読み込むセクタ数
+    mov ch, 0           ; シリンダ 0
+    mov cl, 2           ; セクタ 2 から開始
+    mov dh, 0           ; ヘッド 0
+    ; dl はBIOSが起動ドライブ番号（0x00や0x80）を保持しているのでそのまま使用
+    mov bx, 0x8000      ; ES:BX = 0x0000:0x8000
+    int 0x13
+    jc .error
+
+    ; 2. A20ラインの有効化 (Fast A20)
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    ; 3. GDTのロード
+    cli
+    lgdt [gdt_pointer]
+
+    ; 4. CR0 の PE ビットをセットして保護モードへ
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    ; 5. 32bitコードセグメントへジャンブ
+    jmp 0x08:.pm_entry
+
+.error:
+    mov si, msg_error
+.loop_err:
+    lodsb
+    or al, al
+    jz .halt
+    mov ah, 0x0E
+    int 0x10
+    jmp .loop_err
+.halt:
+    hlt
+    jmp .halt
+
+msg_error: db "Disk Read Error!", 0
 
 ; ============================================================================
-; 定数定義
+; GDT (Global Descriptor Table) 定義
+; ============================================================================
+align 4
+gdt_start:
+    ; ヌル記述子
+    dd 0, 0
+gdt_code:
+    ; ベース: 0x0, リミット: 0xFFFFF, 属性: 32bit, コード, 実行/読込, 特権0
+    dw 0xFFFF, 0x0000
+    db 0x00, 0x9A, 0xCF, 0x00
+gdt_data:
+    ; ベース: 0x0, リミット: 0xFFFFF, 属性: 32bit, データ, 読込/書込, 特権0
+    dw 0xFFFF, 0x0000
+    db 0x00, 0x92, 0xCF, 0x00
+gdt_end:
+
+gdt_pointer:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+; ============================================================================
+; 32bit 保護モード・エントリー
+; ============================================================================
+bits 32
+.pm_entry:
+    ; セグメントレジスタの初期化
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    ; 6. 一時領域(0x8000)から1MB(0x100000)へカーネル本体をコピー
+    ; (15セクタ = 7680バイト = 1920ダブルワード)
+    mov esi, 0x8000
+    mov edi, 0x100000
+    mov ecx, 1920
+    rep movsd
+
+    ; 7. リンカスクリプトで配置した kernel_entry へジャンプ
+    jmp kernel_entry
+
+; ブートセクタ（512バイト）のシグネチャ
+times 510-($-$$) db 0
+dw 0xAA55
+
+; ============================================================================
+; ここからカーネルエントリ（リンカによって 0x100000 に配置される）
 ; ============================================================================
 %define KERNEL_CS       0x08
 %define KERNEL_DS       0x10
 %define KERNEL_STACK_SIZE 8192
 
-; ============================================================================
-; グローバルシンボル
-; ============================================================================
 global kernel_entry
 global load_idt
 global asm_mouse_handler
@@ -17,125 +119,71 @@ global asm_mouse_handler
 extern kernel_main
 extern c_mouse_handler
 
-; ============================================================================
-; .bss セクション（スタック）
-; ============================================================================
-section .bss
-align 16
-__stack_bottom:
-    resb KERNEL_STACK_SIZE
-__stack_top:
-
-; ============================================================================
-; .text.entry セクション（エントリーポイント）
-; ============================================================================
 section .text.entry
 
 kernel_entry:
-    ; 割り込み禁止
     cli
-    
-    ; 方向フラグクリア
     cld
     
-    ; スタックポインタの初期化（スタック最上部）
-    mov esp, __stack_top
+    ; スタックポインタの初期化 [cite: 1]
+    mov esp, __stack_top [cite: 1]
     
-    ; スタックキャナリ設定（オーバーフロー検出用）
-    mov dword [__stack_top - 4], 0xDEADBEEF
+    ; スタックキャナリ設定 [cite: 1]
+    mov dword [__stack_top - 4], 0xDEADBEEF [cite: 1]
     
-    ; カーネルデータセグメントの設定
-    mov ax, KERNEL_DS
-    mov ss, ax
+    ; カーネルデータセグメントの設定 [cite: 1]
+    mov ax, KERNEL_DS [cite: 1]
+    mov ss, ax [cite: 1]
     
-    ; 不定なセグメントレジスタをリセット
-    xor eax, eax
-    mov fs, ax
-    mov gs, ax
+    xor eax, eax [cite: 1]
+    mov fs, ax [cite: 1]
+    mov gs, ax [cite: 1]
     
-    ; カーネルメインへ移行
-    call kernel_main
+    call kernel_main [cite: 1]
     
-    ; 戻ってこない（安全のため無限ループ）
 .halt:
-    cli
-    hlt
-    jmp .halt
+    cli [cite: 1]
+    hlt [cite: 1]
+    jmp .halt [cite: 1]
 
-; ============================================================================
-; .text セクション（プログラムコード）
-; ============================================================================
 section .text
 
-; ============================================================================
-; void load_idt(uint32_t idt_ptr)
-; 
-; IDT (Interrupt Descriptor Table) をロードする
-; 引数: [esp + 4] = IDTPtr 構造体のアドレス
-; ============================================================================
 load_idt:
-    push ebp
-    mov ebp, esp
-    
-    ; idt_ptr アドレスを edx に取得
-    mov edx, [ebp + 8]
-    
-    ; LIDT 命令で IDT をロード
-    lidt [edx]
-    
-    pop ebp
-    ret
+    push ebp [cite: 1]
+    mov ebp, esp [cite: 1]
+    mov edx, [ebp + 8] [cite: 1]
+    lidt [edx] [cite: 1]
+    pop ebp [cite: 1]
+    ret [cite: 1]
 
-; ============================================================================
-; void asm_mouse_handler(void)
-;
-; マウス割り込み（IRQ12）のアセンブラ側エントリーポイント
-; CPU が自動的に以下をスタックに積む:
-;   [esp + 0]: EIP
-;   [esp + 4]: CS
-;   [esp + 8]: EFLAGS
-;
-; iretd で復帰時に自動的に復元される
-; ============================================================================
 asm_mouse_handler:
-    ; 汎用レジスタを保存
-    pushad
+    pushad [cite: 1]
+    push ds [cite: 1]
+    push es [cite: 1]
+    push fs [cite: 1]
+    push gs [cite: 1]
     
-    ; セグメントレジスタを保存
-    push ds
-    push es
-    push fs
-    push gs
+    cld [cite: 1]
     
-    ; 方向フラグクリア（C++ 側の memset や std:: 関数使用時に必要）
-    cld
+    mov ax, KERNEL_DS [cite: 1]
+    mov ds, ax [cite: 1]
+    mov es, ax [cite: 1]
+    mov fs, ax [cite: 1]
+    mov gs, ax [cite: 1]
     
-    ; カーネルデータセグメントの設定
-    mov ax, KERNEL_DS
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
+    sub esp, 4 [cite: 1]
+    call c_mouse_handler [cite: 1]
+    add esp, 4 [cite: 1]
     
-    ; スタックアライメント確保（System V ABI 互換性）
-    ; 32-bit では強制されないが、SSE 命令使用時に重要
-    ; call 直前のスタックポインタが 16 バイトアライメント - 4 であること
-    sub esp, 4
-    
-    ; C++ 側のマウスハンドラを呼び出し
-    call c_mouse_handler
-    
-    ; スタックアライメントを復元
-    add esp, 4
-    
-    ; セグメントレジスタを復元
-    pop gs
-    pop fs
-    pop es
-    pop ds
-    
-    ; 汎用レジスタを復元
-    popad
-    
-    ; 割り込みから復帰（EIP, CS, EFLAGS を自動復元）
-    iretd
+    pop gs [cite: 1]
+    pop fs [cite: 1]
+    pop es [cite: 1]
+    pop ds [cite: 1]
+    popad [cite: 1]
+    iretd [cite: 1]
+
+section .bss
+align 16
+__stack_bottom:
+    resb KERNEL_STACK_SIZE [cite: 1]
+__stack_top:
